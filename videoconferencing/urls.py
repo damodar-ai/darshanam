dcaa82f24374e76023da31eac7f3d2b11fae8369
DAMODAR TIWARI <117446972+Damodar12345678@users.noreply.github.com>
2023-09-11 11:20:18 +0530

    Update urls.py

--- videoconferencing/urls.py
+++ videoconferencing/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 # from django.urls import path, include
 from django.urls import path
-from videoconference import views
+from videoconference_app import views
 from django.conf import settings
 from django.conf.urls.static import static
 
