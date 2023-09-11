7659151cc0c88f046e56d4716b477ce4029aa81b
DAMODAR TIWARI <117446972+Damodar12345678@users.noreply.github.com>
2023-09-11 11:24:27 +0530

    Update urls.py

--- videoconferencing/urls.py
+++ videoconferencing/urls.py
@@ -27,8 +27,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('videoconference_app.urls')
-    )
+    path('', 'videoconference_app.urls')
 ]
 
 
